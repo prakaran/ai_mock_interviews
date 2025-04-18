5fb4439c968f56c57d1aa3e05d3aedbcbce9f8da
prakaran <prakaranbhatt@gmail.com>
2025-04-18 12:45:17 +0545

    setup app and auth pages

---
+++ app/(auth)/sign-in/page.tsx
@@ -0,0 +1,12 @@
+import AuthForm from "@/components/AuthForm";
+import React from "react";
+
+const LoginPage = () => {
+  return (
+    <>
+      <AuthForm type="sign-in" />
+    </>
+  );
+};
+
+export default LoginPage;
---
+++ app/(auth)/sign-up/page.tsx
@@ -0,0 +1,12 @@
+import AuthForm from "@/components/AuthForm";
+import React from "react";
+
+const RegisterPage = () => {
+  return (
+    <>
+      <AuthForm type="sign-up" />
+    </>
+  );
+};
+
+export default RegisterPage;
